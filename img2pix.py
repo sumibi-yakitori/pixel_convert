0f570cf27443528ed8e3adccfe91d77a8713bbce
yakitori <iongenerator@gmail.com>
2018-12-27 09:37:31 +0900

    no message

---
+++ img2pix.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+import os
+import cv2
+import sys
+from pixel import make_dot
+
+argvs = sys.argv
+img_path = argvs[1]
+
+k = 128
+scale = 6
+# 0, 50, 100, 200
+blur = 100
+# 輪郭線膨張
+erode = 1
+# 透過PNGで書き出す
+alpha = False
+# Twitter用に1pxだけ透過
+to_tw = False
+
+img_res, colors = make_dot(
+    img_path,
+    k=k,
+    scale=scale,
+    blur=blur,
+    erode=erode,
+    alpha=alpha,
+    to_tw=to_tw
+)
+
+img_name = 'output'
+result_path = os.path.join('./', img_name + '.png')
+cv2.imwrite(result_path, img_res)
